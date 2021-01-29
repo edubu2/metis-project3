71d2cbc40bc902b198b8ebf43517c3e95f4adb60
Elliot Wilens <wilensel@gmail.com>
2021-01-28 18:37:53 -0600

    Restructure query_dfs.py and add prod_detail DF

--- code/query_dfs.py
+++ code/query_dfs.py
@@ -24,60 +24,75 @@ def save_dfs(conn):
     """
     c = conn.cursor()
 
-    # create df_orders
-    query = """
-            SELECT *
-            FROM orders;
+    def query_orders(conn):
+        """ Creates df_orders. """
+
+        query = """
+                    SELECT *
+                    FROM orders;
+                """
+
+        df_orders = pd.read_sql_query(query, conn)
+
+        return query_orders
+
+    def get_train(conn):
+        """ Creates df_train. """
+
+        q = """
+                SELECT train_orders.order_id, train_orders.product_id,
+                    train_orders.add_to_cart_order, train_orders.reordered,
+                    map.user_id, map.eval_set, map.order_number,
+                    map.order_dow, map.order_hour_of_day, map.days_since_prior_order
+                FROM train_orders
+                LEFT JOIN
+                    (
+                        SELECT * FROM orders
+                    ) AS map
+                ON train_orders.order_id = map.order_id
             """
 
-    df_orders = pd.read_sql_query(query, conn)
+        df_train = pd.read_sql_query(q, conn)
 
-    # create df_train
+        return df_train
 
-    q = """
-        SELECT train_orders.order_id, train_orders.product_id,
-            train_orders.add_to_cart_order, train_orders.reordered,
-            map.user_id, map.eval_set, map.order_number,
-            map.order_dow, map.order_hour_of_day, map.days_since_prior_order
-        FROM train_orders
-        LEFT JOIN
-            (
-                SELECT * FROM orders
-            ) AS map
-        ON train_orders.order_id = map.order_id
-        """
+    def get_prior(conn):
+        """Creates df_prior. """
 
-    df_train = pd.read_sql_query(q, conn)
+        q = """
+                SELECT prior_orders.order_id, prior_orders.product_id,
+                    prior_orders.add_to_cart_order, prior_orders.reordered,
+                    map.user_id, map.eval_set, map.order_number,
+                    map.order_dow, map.order_hour_of_day, map.days_since_prior_order
+                FROM prior_orders
+                LEFT JOIN
+                    (
+                        SELECT * FROM orders
+                    ) AS map
+                ON prior_orders.order_id = map.order_id
+            """
 
-    # create df_prior
+        df_prior = pd.read_sql_query(q, conn)
 
-    q = """
-        SELECT prior_orders.order_id, prior_orders.product_id,
-            prior_orders.add_to_cart_order, prior_orders.reordered,
-            map.user_id, map.eval_set, map.order_number,
-            map.order_dow, map.order_hour_of_day, map.days_since_prior_order
-        FROM prior_orders
-        LEFT JOIN
-            (
-                SELECT * FROM orders
-            ) AS map
-        ON prior_orders.order_id = map.order_id
-        """
+        return df_prior
 
-    df_prior = pd.read_sql_query(q, conn)
+    def get_prod_detail(conn):
+        """Creates df_prod. """
 
-    # create df_prior
+        q = "SELECT product_id, product_name, aisle_id, products.department_id, dep FROM products"
+        df_prod = pd.read_sql_query(q, conn)
+        return df_prod
 
-    q = "SELECT * FROM products"
-    df_prod = pd.read_sql_query(q, conn)
+    def main():
+        
 
-    return df_orders, df_train, df_prior, df_prod
+    return df_orders, df_train, df_prior, df_prod_detail
 
 
 def get_dfs():
     conn = establish_conn()
-    df_orders, df_train, df_prior, df_prod = save_dfs(conn)
-    return df_orders, df_train, df_prior, df_prod
+    df_orders, df_train, df_prior, df_prod_detail = save_dfs(conn)
+    return df_orders, df_train, df_prior, df_prod_detail
 
 
 if __name__ == "__main__":
