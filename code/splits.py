af6fd31004a1728ceba1692a8e112a208d8e35e0
Elliot Wilens <wilensel@gmail.com>
2021-02-01 17:45:59 -0600

    Fix imports in splits.py

--- code/splits.py
+++ code/splits.py
@@ -2,6 +2,8 @@ from collections import OrderedDict
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+import pandas as pd
+import numpy as np
 
 
 def split_users(df, test_size=0.2, seed=36):
